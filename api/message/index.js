43a9d5b423990668407baae67c02e1a948c8b68d
oksana-blonskaya <oksana.blonskaya@dataart.com>
2024-04-10 16:16:05 +0300

    Added API

---
+++ api/message/index.js
@@ -0,0 +1,5 @@
+module.exports = async function(context, request) {
+    context.res.json({
+        text: "Hello from the API"
+    })
+}
--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>Hello {{ value }}</div>
+  <div>
+    <h2>My First Azure Static Web App</h2>
+    <div>{{ message }}</div>
+  </div>
 </template>
 
 <script>
+
 export default {
   name: "App",
   data() {
     return {
-      value: "World"
+      message: ""
     };
+  },
+  async mounted() {
+    const { text } = await(await fetch("/api/message")).json();
+    this.message = text;
   }
 };
 </script>
